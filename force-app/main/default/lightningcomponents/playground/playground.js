d6ade5568b59b4677da77ac7ed72a1b47aaf23cf
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-10-17 12:51:30 +1100

    Add and display account name by using getFieldValue

--- force-app/main/default/lightningcomponents/playground/playground.js
+++ force-app/main/default/lightningcomponents/playground/playground.js
@@ -1,12 +1,16 @@
 /* Primitives from LWC framework */
 import { Element, api, wire } from 'engine';
-import { restoreRecordId } from 'c-utils';
+import { restoreRecordId, getFieldValue } from 'c-utils';
 /* Object and field schema */
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 import ANNUAL_REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import OWNER_ID_FIELD from '@salesforce/schema/Account.OwnerId';
+
+/* Wire adapter to fetch record data */
+import { getRecord } from 'lightning-ui-api-record';
+import NAME_FIELD from '@salesforce/schema/Account.Name';
 
 
 export default class Playground extends Element {
@@ -26,4 +30,14 @@ export default class Playground extends Element {
     industryField = INDUSTRY_FIELD;
     annualRevenueField = ANNUAL_REVENUE_FIELD;
     ownerIdField = OWNER_ID_FIELD;
+
+
+    /* Load Account.Name for custom visualization */
+    @wire(getRecord, { recordId: '$recordId', fields: [NAME_FIELD] })
+    record;
+
+    /** Gets the Account.Name value. */
+    get nameValue() {
+        return this.record.data ? getFieldValue(this.record.data, NAME_FIELD).value : '';
+    }
 }
