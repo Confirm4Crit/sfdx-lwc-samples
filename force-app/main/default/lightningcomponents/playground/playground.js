3e03c48af0a9eefd2dfae4f270257880d343f0da
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-10-17 12:47:13 +1100

    Add restoreRecordId to get the record context

--- force-app/main/default/lightningcomponents/playground/playground.js
+++ force-app/main/default/lightningcomponents/playground/playground.js
@@ -1,5 +1,6 @@
 /* Primitives from LWC framework */
 import { Element, api, wire } from 'engine';
+import { restoreRecordId } from 'c-utils';
 /* Object and field schema */
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import WEBSITE_FIELD from '@salesforce/schema/Account.Website';
@@ -7,9 +8,17 @@ import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 import ANNUAL_REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import OWNER_ID_FIELD from '@salesforce/schema/Account.OwnerId';
 
+
 export default class Playground extends Element {
     /** Id of record to display. */
     @api recordId;
+
+    /**
+     * Lifecycle method. Restore the record id, if it exists, for improved cross-tab navigation.
+     */
+    connectedCallback() {
+        this.recordId = restoreRecordId();
+    }
 
     /* Expose schema objects/fields to the template. */
     accountObject = ACCOUNT_OBJECT;
